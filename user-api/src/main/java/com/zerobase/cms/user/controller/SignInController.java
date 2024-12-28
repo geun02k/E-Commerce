e7e39cac3215096e066485b3ea5ba24d21b797ee
geun02k <geun02k@gmail.com>
2024-12-29 06:05:37 +0900

    feat : [004-LOGIN-CUSTOMER] 고객 로그인 토큰생성기능 구현

---
+++ user-api/src/main/java/com/zerobase/cms/user/controller/SignInController.java
@@ -0,0 +1,23 @@
+package com.zerobase.cms.user.controller;
+
+import com.zerobase.cms.user.application.SignInApplication;
+import com.zerobase.cms.user.domain.SignInForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/signin")
+@RequiredArgsConstructor
+public class SignInController { // 토큰을 발행해 실제로 접근할 수 있는 근거 생성
+
+    private final SignInApplication signInApplication;
+
+    @PostMapping("/customer")
+     public ResponseEntity<String> customerSignIn(@RequestBody SignInForm form) {
+        return ResponseEntity.ok(signInApplication.customerLoginToken(form));
+    }
+}
